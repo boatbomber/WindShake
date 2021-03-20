f889ac67ff015fb5751b59fa876c7e64d0a32ad4
Max <clonet1019@gmail.com>
2021-03-19 21:15:19 -0500

    Large refactor to work with attributes and tags, smoother motion.
    I reworked the logic of this module to derive its configurations from attributes, and its mounting from tags. The previous interface is still in-tact and preserves backwards compatibility with the new attribute logic.

---
+++ WindShake/Settings.lua
@@ -0,0 +1,32 @@
+local Settings = {}
+
+function Settings.new(object, base)
+	local inst = 
+	{
+		_object = object;
+		_base = base;
+	}
+	
+	return setmetatable(inst, Settings)
+end
+
+function Settings:__index(key)
+	local result = self._object:GetAttribute(key)
+	local base = self._base
+	
+	if base ~= nil then
+        base = base[key]
+        
+		if typeof(result) ~= typeof(base) then
+			result = base
+		end
+	end
+	
+	return result
+end
+
+function Settings:__newindex(key, value)
+	self._object:SetAttribute(key, value)
+end
+
+return Settings
