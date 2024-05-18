cbe3b7b2e5af8d5099439848da896c2611293f4d
Max <clonet1019@gmail.com>
2024-05-17 19:25:12 -0500

    Fully --!strict and type annotated!

--- src/Settings.lua
+++ src/Settings.lua
@@ -1,13 +1,20 @@
+--!strict
 local Settings = {}
 
-local SettingTypes = {
-	WindPower = "number",
-	WindSpeed = "number",
-	WindDirection = "Vector3",
-	PivotOffset = "CFrame",
+export type Class = {
+	Destroy: (any) -> (),
+	WindPower: number?,
+	WindSpeed: number?,
+	PivotOffset: CFrame?,
+	WindDirection: Vector3?,
+	PivotOffsetInverse: CFrame?,
 }
 
-function Settings.new(object: BasePart | Bone | ModuleScript)
+local function Normalize(vec: Vector3)
+	return vec.Magnitude > 0 and vec.Unit or Vector3.zero
+end
+
+function Settings.new(object: BasePart | Bone | ModuleScript): Class
 	local objectSettings = {}
 
 	-- Initial settings
@@ -15,54 +22,59 @@ function Settings.new(object: BasePart | Bone | ModuleScript)
 	local WindSpeed = object:GetAttribute("WindSpeed")
 	local WindDirection = object:GetAttribute("WindDirection")
 
-	objectSettings.WindPower = if typeof(WindPower) == SettingTypes.WindPower then WindPower else nil
-	objectSettings.WindSpeed = if typeof(WindSpeed) == SettingTypes.WindSpeed then WindSpeed else nil
-	objectSettings.WindDirection = if typeof(WindDirection) == SettingTypes.WindDirection
-		then (if WindDirection.Magnitude > 0 then WindDirection.Unit else Vector3.zero)
+	objectSettings.WindPower = if type(WindPower) == "number"
+		then WindPower
 		else nil
-	objectSettings.PivotOffset = if object:IsA("BasePart") then object.PivotOffset else nil
-	objectSettings.PivotOffsetInverse = if typeof(objectSettings.PivotOffset) == "CFrame"
+
+	objectSettings.WindSpeed = if type(WindSpeed) == "number"
+		then WindSpeed
+		else nil
+
+	objectSettings.WindDirection = if typeof(WindDirection) == "Vector3"
+		then Normalize(WindDirection)
+		else nil
+
+	objectSettings.PivotOffset = if object:IsA("BasePart")
+		then object.PivotOffset
+		else nil
+
+	objectSettings.PivotOffsetInverse = if objectSettings.PivotOffset
 		then objectSettings.PivotOffset:Inverse()
 		else nil
 
 	-- Update settings on event
+	local Conns = {} :: {
+		[string]: RBXScriptConnection
+	}
 
-	local PowerConnection = object:GetAttributeChangedSignal("WindPower"):Connect(function()
+	Conns.PowerConnection = object:GetAttributeChangedSignal("WindPower"):Connect(function()
 		WindPower = object:GetAttribute("WindPower")
-		objectSettings.WindPower = if typeof(WindPower) == SettingTypes.WindPower then WindPower else nil
+		objectSettings.WindPower = if type(WindPower) == "number" then WindPower else nil
 	end)
 
-	local SpeedConnection = object:GetAttributeChangedSignal("WindSpeed"):Connect(function()
+	Conns.SpeedConnection = object:GetAttributeChangedSignal("WindSpeed"):Connect(function()
 		WindSpeed = object:GetAttribute("WindSpeed")
-		objectSettings.WindSpeed = if typeof(WindSpeed) == SettingTypes.WindSpeed then WindSpeed else nil
+		objectSettings.WindSpeed = if type(WindSpeed) == "number" then WindSpeed else nil
 	end)
 
-	local DirectionConnection = object:GetAttributeChangedSignal("WindDirection"):Connect(function()
+	Conns.DirectionConnection = object:GetAttributeChangedSignal("WindDirection"):Connect(function()
 		WindDirection = object:GetAttribute("WindDirection")
-		objectSettings.WindDirection = if typeof(WindDirection) == SettingTypes.WindDirection
-			then (if WindDirection.Magnitude > 0 then WindDirection.Unit else Vector3.zero)
-			else nil
+		objectSettings.WindDirection = if typeof(WindDirection) == "Vector3" then Normalize(WindDirection) else nil
 	end)
 
-	local PivotConnection
 	if object:IsA("BasePart") then
-		PivotConnection = object:GetPropertyChangedSignal("PivotOffset"):Connect(function()
-			objectSettings.PivotOffset = object.PivotOffset
-			objectSettings.PivotOffsetInverse = objectSettings.PivotOffset:Inverse()
+		Conns.PivotConnection = object:GetPropertyChangedSignal("PivotOffset"):Connect(function()
+			local pivotOffset = object.PivotOffset
+			objectSettings.PivotOffset = pivotOffset
+			objectSettings.PivotOffsetInverse = pivotOffset:Inverse()
 		end)
 	end
 
 	-- Cleanup function for when shake is removed or object is unloaded
-
-	function objectSettings:Destroy()
-		PowerConnection:Disconnect()
-		SpeedConnection:Disconnect()
-		DirectionConnection:Disconnect()
-		if PivotConnection then
-			PivotConnection:Disconnect()
+	function objectSettings.Destroy(_self: any)
+		for i, conn in pairs(Conns) do
+			conn:Disconnect()
 		end
-
-		table.clear(objectSettings)
 	end
 
 	return objectSettings
